c5a53e15ab723492f8b8a15b09b3201b6f09557f
Ronnie C <romeechavhan@gmail.com>
2025-05-22 10:22:59 +0530

    add home route

--- src/app.js
+++ src/app.js
@@ -13,6 +13,9 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the School API");
+});
 // Import routes
 import schoolRoutes from "./Routes/schoolRoutes.js";
 
